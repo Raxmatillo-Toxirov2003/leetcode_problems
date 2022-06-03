cd9cca3357c530fb78ea4c1055c0ad36023db707
Raxmatillo Toxirov <raxmatillotoxirov2003@gmail.com>
2022-06-04 02:06:56 +0500

    fsa

---
+++ addstrings/index.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+
+    let y = Number(num1) + Number(num2);
+    let s = y.toString()
+    return s;
+};
