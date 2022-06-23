5b97b0414f03129c375cfb0271494cd77516214f
Raxmatillo Toxirov <raxmatillotoxirov2003@gmail.com>
2022-06-23 23:55:46 +0500

    23.06.2022

---
+++ restoreString/restoreString.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    const result = []
+
+    for (let i = 0; i < s.length; i++) {
+        const letter = s[i]
+        const index = indices[i]
+        result[index] = letter
+    }
+
+    return result.join('')
+};
