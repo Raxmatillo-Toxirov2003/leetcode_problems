0caa4fc5ea65d2a3f3feefbadc794b9c6612905e
Raxmatillo Toxirov <raxmatillotoxirov2003@gmail.com>
2022-06-24 23:59:37 +0500

    24.06.2022

---
+++ toLowerCase/toLowerCase.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    let output = '';
+    for (let i = 0; i < s.length; i++) {
+        let code = s.charCodeAt(i);
+        if (65 <= code && code <= 90) {
+            output += String.fromCharCode(code + 32);
+        } else {
+            output += s[i];
+        }
+    }
+    return output;
+};
