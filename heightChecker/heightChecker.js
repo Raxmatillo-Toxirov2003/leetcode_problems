3540b0b9f4d7c556242b6858ffd75a1003298c66
Raxmatillo Toxirov <raxmatillotoxirov2003@gmail.com>
2022-06-25 23:57:43 +0500

    25.06.2022

---
+++ heightChecker/heightChecker.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    const sortedHeights = [...heights].sort((a, b) => a - b)
+    let count = 0
+
+    for (let i = 0; i < heights.length; i++) {
+        if (heights[i] !== sortedHeights[i]) {
+            count++
+        }
+    }
+
+    return count
+};
