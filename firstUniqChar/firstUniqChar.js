f03d211e1f1ba179e10636eee96b03eb26274a55
Raxmatillo Toxirov <raxmatillotoxirov2003@gmail.com>
2022-07-02 23:49:08 +0500

    02.07.2022

---
+++ firstUniqChar/firstUniqChar.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    for (let i of s) {
+        if (s.lastIndexOf(i) == s.indexOf(i)) {
+            return s.indexOf(i);
+        }
+    }
+    return -1;
+};
